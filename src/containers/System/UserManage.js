c8c435a47fd13be5272a81552dacdface3e14cc7
Quang Hoang <hoang.tq@tjtech.tech>
2022-10-20 18:15:33 +0700

    CRUD react 5

--- src/containers/System/UserManage.js
+++ src/containers/System/UserManage.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { FormattedMessage } from 'react-intl';
 import { connect } from 'react-redux';
 import "./UserManage.scss";
-import {getAllUser} from '../../services/userService'
+import {getAllUser, createNewUserService} from '../../services/userService'
 import ModalUser from './ModalUser';
 class UserManage extends Component {
 
@@ -15,6 +15,10 @@ class UserManage extends Component {
     }
 
     async componentDidMount() {
+        await this.getAllUsersFromReact()
+    }
+
+    getAllUsersFromReact = async () => {
         let response = await getAllUser('ALL')
         if(response && response.errCode === 0) {
             this.setState({
@@ -35,6 +39,22 @@ class UserManage extends Component {
         })
     }
 
+    createNewUser = async(data) => {
+        try {
+            let response = await createNewUserService(data)
+            if(response && response.errCode !== 0) {
+                alert(response.errMessage)
+            } else {
+                await this.getAllUsersFromReact()
+                this.setState({
+                    isOpenModalUser: false
+                })
+            }
+        } catch (e) {
+            console.log(e)
+        }
+    }
+
     render() {
         let arrUsers = this.state.arrUsers
         return (
@@ -42,6 +62,7 @@ class UserManage extends Component {
                 <ModalUser
                     isOpen={this.state.isOpenModalUser}
                     toggleFromParent={this.toggleUserModal}
+                    createNewUser = {this.createNewUser}
                 />
                 <div className='title text-center'>MANAGE USERS WITH ERIC</div>
                 <div className='mx-1'>
@@ -51,28 +72,30 @@ class UserManage extends Component {
                 </div>
                 <div className='users-table mt-4 mx-1'>
                 <table id="customers">
-                    <tr>
-                        <th>Email</th>
-                        <th>First name</th>
-                        <th>Last name</th>
-                        <th>Address</th>
-                        <th>Action</th>
-                    </tr>
-                    { arrUsers && arrUsers.map((item, index) => {
-                        return(
-                            <tr>
-                                <td>{item.email}</td>
-                                <td>{item.firstName}</td>
-                                <td>{item.lastName}</td>
-                                <td>{item.address}</td>
-                                <td>
-                                    <button className='btn-edit'><i className='fas fa-pencil-alt'></i></button>
-                                    <button className='btn-delete'><i className='fas fa-trash'></i></button>
-                                </td>
-                            </tr>
-                        )
-                    })
-                    }
+                    <tbody>
+                        <tr>
+                            <th>Email</th>
+                            <th>First name</th>
+                            <th>Last name</th>
+                            <th>Address</th>
+                            <th>Action</th>
+                        </tr>
+                        { arrUsers && arrUsers.map((item, index) => {
+                            return(
+                                <tr>
+                                    <td>{item.email}</td>
+                                    <td>{item.firstName}</td>
+                                    <td>{item.lastName}</td>
+                                    <td>{item.address}</td>
+                                    <td>
+                                        <button className='btn-edit'><i className='fas fa-pencil-alt'></i></button>
+                                        <button className='btn-delete'><i className='fas fa-trash'></i></button>
+                                    </td>
+                                </tr>
+                            )
+                        })
+                        }
+                    </tbody>
                 </table>
                 </div>
             </div>
